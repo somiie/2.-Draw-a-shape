051ac4fef5b47006b27f9db673e71bb08e18984f
Somie1 <somelezesipayile@gmail.com>
2024-01-15 09:22:29 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Draw_a_shape
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("     /|");
+            Console.WriteLine("    / |");
+			Console.WriteLine("   /  |");
+			Console.WriteLine("  /   |");
+            Console.WriteLine("  ----");
+
+            Console.Write("Watch where the cursor will be");
+
+			//Freeze console
+			Console.ReadLine();
+        }
+    }
+}
